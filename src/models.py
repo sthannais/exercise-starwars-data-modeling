658a125e68649e6e6c156f52bc386ac0aab60875
Thannais <superlanoneri@gmail.com>
2022-12-04 20:41:47 +0000

    Starwars Data blog

--- src/models.py
+++ src/models.py
@@ -8,23 +8,48 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    mail = Column(String(50))
+    password = Column(String(50))
+    subscription_date = Column(String(50))
+    first_name = Column(String(20))
+    last_name = Column(String(20))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Characters(Base):
+    __tablename__ = 'characters'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(50))
+    height = Column(String(50))
+    hair_color = Column(String(50))
+    skin_color = Column(String(50))
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20))
+    rotation_period = Column(String(20))
+    climate = Column(String(20))
+    terrain = Column(String(20))
+    populations = Column(String(20))
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+class FavoriteCharacters(Base): 
+    __tablename__ = 'favorite_characters'
+    id = Column(Integer, primary_key=True)
+    characters_id = Column(Integer, ForeignKey('characters.id'))
+    create_at = Column(String(50))
+
+class FavoritesPlanets(Base):
+    __tablename__ = 'favorites_planets'
+    id = Column(Integer, primary_key=True)
+    planest_id = Column(Integer, ForeignKey('planets.id'))
+    create_at = Column(String(50))
+
+
 
     def to_dict(self):
         return {}
